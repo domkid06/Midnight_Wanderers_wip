ace9d6b7d9d3b71db80697836f93726d8652168d
domkid06 <dom.brunson@gmail.com>
2026-05-31 18:32:07 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+    romName: "midnight_neo_0031", // Name of the ROM file (without .zip extension)
+    gameTitle: "Midnight Wanderers", // Game title (displayed in the browser tab)
+    gameUrl: "https://z-team.itch.io/midnight-wanderers" // Link to the official game page
+};
