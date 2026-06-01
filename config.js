5528506e82d0181b93558c4b4add3ddafd6b2565
domkid06 <dom.brunson@gmail.com>
2026-06-02 00:11:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 const CONFIG = {
     romName: "midnight_neo_0031", // Name of the ROM file (without .zip extension)
     gameTitle: "Midnight Wanderers", // Game title (displayed in the browser tab)
-    gameUrl: "https://z-team.itch.io/midnight-wanderers" // Link to the official game page
+    gameUrl: "https://z-team.itch.io/midnight-wanderers", // Link to the official game page
+    linkText: "Midnight Wanderers on itch.io", // Text for the link
+    linkColor: "#F45E33" // Color for the link (hex code, color name, etc.)
 };
